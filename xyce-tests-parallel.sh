f3d861662e7c68ce7466ac49a89373cc651d2aa4
Sebastian Tabares Amaya <sytabaresa@gmail.com>
2017-06-25 19:40:17 -0500

    test corrections and optimize Docker image size

--- xyce-tests-parallel.sh
+++ xyce-tests-parallel.sh
@@ -1,7 +1,7 @@
-#!/bin/Bash
+#!/bin/bash
 
-./Xyce_Regression/TestScripts/run_xyce_regression \
+$XYCE_SCRDIR/Xyce_Regression/TestScripts/run_xyce_regression \
 --output=`pwd`/Xyce_Test --xyce_test="./Xyce_Regression" \
 --taglist="+parallel+nightly?noverbose-klu-verbose?fft" \
 --resultfile=`pwd`/parallel_results \
-"mpirun -np 2 $XYCE_SRCDIR/Xyce/src/Xyce" 
+"mpirun -np 2 `pwd`/src/Xyce" 
