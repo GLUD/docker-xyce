9b042e6e0b1d531f4a5592aa7188c4e0ec819e05
Sebastian Tabares Amaya <sytabaresa@gmail.com>
2017-06-25 22:48:56 -0500

    libfabric-1.3.0 bug correction with update package and test corrections

--- xyce-tests-parallel.sh
+++ xyce-tests-parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$XYCE_SCRDIR/Xyce_Regression/TestScripts/run_xyce_regression \
---output=`pwd`/Xyce_Test --xyce_test="./Xyce_Regression" \
+$XYCE_SRCDIR/Xyce_Regression/TestScripts/run_xyce_regression \
+--output=`pwd`/Xyce_Test --xyce_test="$XYCE_SRCDIR/Xyce_Regression" \
 --taglist="+parallel+nightly?noverbose-klu-verbose?fft" \
 --resultfile=`pwd`/parallel_results \
 "mpirun -np 2 `pwd`/src/Xyce" 
